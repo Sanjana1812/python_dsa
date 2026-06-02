3ef19a8014d5969e69af868da5dfe8adf40c09ad
sanjana <125641551+Sanjana1812@users.noreply.github.com>
2026-06-02 07:02:01 -0400

    Create dsaQ12.py
    Added Finding Missing Number

---
+++ dsaQ12.py
@@ -0,0 +1,11 @@
+dsaQ12: Find missing number in [1..n]
+
+arr = [1,2,4,5]
+expected=sum(range(1,6))
+actual=sum(arr)
+missing = expected- actual
+print("find missing number:",missing)
+
+output:
+find missing number: 3
+
