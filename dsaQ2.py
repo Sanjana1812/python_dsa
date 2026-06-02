bb465a3abca4b2f204a0c22af908965d91890cdb
sanjana <125641551+Sanjana1812@users.noreply.github.com>
2026-06-02 06:40:43 -0400

    Create dsaQ2.py
    Add largest element array solution

---
+++ dsaQ2.py
@@ -0,0 +1,11 @@
+dsaQ2: Find the largest element in an array
+
+arr = [10, 50, 20, 99, 40]
+largest = arr[0]
+for i in arr:
+  if i > largest:
+    largest=i
+print("largest element is:", largest)
+
+output:
+largest element is: 99
